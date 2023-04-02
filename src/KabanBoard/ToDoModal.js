fa559be8419cc25c6afd70c367ad3326ee9f4268
mihirprad <mihirprad@hotmail.com>
2023-04-02 12:24:40 +1000

    improved modal styling
    centred the modal.
    Added overlay

--- src/KabanBoard/ToDoModal.js
+++ src/KabanBoard/ToDoModal.js
@@ -8,12 +8,26 @@ Modal.setAppElement('#root');
 export default function ToDoModal(){
     const modalContext = useContext(ToDoModalContext);
 
-
+    const modalStyle = {
+      content: {
+        top: '45%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        backgroundColor: '#fff',
+        borderRadius: '10px',
+        padding: '20px',
+        height: "70%",
+        width: "80%"
+      },
+      overlay: {
+        backgroundColor: 'rgba(0, 0, 0, 0.5)'
+      }
+    };
     return (    <>
-    <div><Modal isOpen={modalContext.modalIsOpen} onRequestClose={modalContext.handleCloseModal}  className="to-do-modal" >
+    <Modal isOpen={modalContext.modalIsOpen} onRequestClose={modalContext.handleCloseModal}   style= {modalStyle}>
           <h2>{modalContext.selectedToDoItem.content}</h2>
           <button onClick={modalContext.handleCloseModal}>Close Modal</button>
-        </Modal></div>
+        </Modal>
         
       </>)
 
